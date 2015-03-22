5fabf770b997ac2181af64ebe06cdc9a8a96b9c2
Crandel <cradlemann@gmail.com>
2015-03-22 10:40:11 +0200

    add sqlite create table

--- password.py
+++ password.py
@@ -6,49 +6,58 @@ import sys, argparse
 
 class Password():
 
-    def connect():
+    def connect(self):
         with lite.connect('pass.db') as con:
-            return con
+            cur = con.cursor()
+            cur.execute('''CREATE TABLE IF NOT EXISTS password
+             (email text, password text, login text, site text, description text)''')
+            cur.fetchone()
 
-    def search(args):
+            return cur
+
+    def search(self, args):
         # args validators
         email = str(args.e)
         password = str(args.p)
-        print(self.connect)
+        cur = self.connect()
         print(email)
         print('search pass')
 
-    def add(args):
+    def add(self, args):
         # args validators
         email = str(args.e)
         password = str(args.p)
+        cur = self.connect()
         print(email)
         print('add pass')
 
-    def delete(args):
+    def delete(self, args):
         # args validators
         email = str(args.e)
         password = str(args.p)
+        cur = self.connect()
         print(email)
         print('delete pass')
 
 
 def parse_args():
     print('start parse args')
+    password = Password()
     parser = argparse.ArgumentParser(description='Password database utility')
     parser.add_argument('-e', help='use to add email or search on it')
     parser.add_argument('-l', help='use to add login or search on it')
+    parser.add_argument('-s', help='use to add site or search on it')
     parser.add_argument('-p', help='use to add password')
     parser.add_argument('-d', help='use to add short description or search on it with regexp')
-    parser.set_defaults(func=Password.search)
+    parser.set_defaults(func=password.search)
 
     subparsers = parser.add_subparsers()
 
     parser_add = subparsers.add_parser('add', help='Add new password to database')
-    parser_add.set_defaults(func=Password.add)
+    parser_add.set_defaults(func=password.add)
     
     parser_delete = subparsers.add_parser('delete', help='Delete password from database')
-    parser_delete.set_defaults(func=Password.delete)
+    parser_delete.set_defaults(func=password.delete)
     
     print('end parse')
     return parser.parse_args()
@@ -58,7 +67,7 @@ def main():
     print('start')
     args = parse_args()
     print('-----')
-    args.func(args)
+    args.func(args=args)
     print('stop')
 
 
