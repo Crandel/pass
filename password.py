8abe7094bb6212a289c30b01b144a09215005296
Crandel <cradlemann@gmail.com>
2015-08-02 12:41:36 +0300

    refactored database operations into class

--- password.py
+++ password.py
@@ -11,6 +11,125 @@ SITE = 'site'
 DESCRIPTION = 'description'
 
 
+class Db(object):
+
+    def __init__(self, *args, **kwargs):
+
+        with sqlite3.connect('pass.db') as con:
+
+            self.con = con
+            cur = con.cursor()
+            try:
+                cur.execute('SELECT * FROM {}'.format(TABLE))
+            except sqlite3.OperationalError as e:
+                # CREATE TABLE IF NOT EXISTS password (id INTEGER PRIMARY KEY ASC, email text, password text, login text, site text, description text)
+                query = 'CREATE TABLE IF NOT EXISTS {0} (id INTEGER PRIMARY KEY ASC, {1} text, {2} text, {3} text, {4} text, {5} text)'.format(
+                    TABLE, EMAIL, PASSWORD, LOGIN, SITE, DESCRIPTION
+                )
+                cur.execute(query)
+            con.commit()
+            self.cursor = cur
+
+    def __del__(self, *args, **kwargs):
+
+        self.cursor.close()
+        self.con.close()
+
+    def search(self, *args, **kwargs):
+        params = kwargs['params']
+        sql_get = self.search_query(params)
+        cur = self.cursor
+        results = False
+
+        try:
+            cur.execute(sql_get, params)
+            results = cur.fetchall()
+        except sqlite3.Error as e:
+            print(e, 'err')
+
+        return results
+
+    def search_query(self, params):
+
+        sql_get = 'SELECT * FROM {}'.format(TABLE)
+
+        if params:
+            params_sql = ' WHERE'
+            i = False
+            for p in params:
+                if i:
+                    params_sql += ' OR'
+                params_sql += ' {0}=:{1}'.format(p, p)
+                i = True
+            sql_get += params_sql
+        sql_get += ';'
+
+        return sql_get
+
+    def add(self, *args, **kwargs):
+        params = kwargs['params']
+        sql_add = self.add_query(params)
+        cur = self.cursor
+
+        try:
+            cur.execute(sql_add, params)
+            self.con.commit()
+        except sqlite3.Error as e:
+            print(e, 'error')
+        print('Record was added')
+
+    def add_query(self, params):
+
+        # INSERT INTO password(password, site, login, description) VALUES (:password, :site, :login, :description);
+        sql_add = 'INSERT INTO ' + TABLE
+        if params:
+            name = ''
+            values = ''
+            i = False
+            for p in params:
+                if i:
+                    name += ', '
+                    values += ', '
+                name += p
+                values += ':' + p
+                i = True
+            sql_add = sql_add + '(' + name + ') VALUES (' + values + ')'
+        sql_add += ';'
+
+        return sql_add
+
+    def delete(self, *args, **kwargs):
+        cur = self.cursor
+        params = kwargs['params']
+        rows = kwargs['rows']
+
+        for r in rows:
+            sql, par = self.delete_query(params, r)
+            try:
+                cur.execute(sql, par)
+            except sqlite3.Error as e:
+                print(e)
+            self.con.commit()
+        print('delete pass')
+
+    def delete_query(self, params, r):
+
+        if EMAIL in params:
+            i = [1, EMAIL]
+        elif LOGIN in params:
+            i = [3, LOGIN]
+        elif SITE in params:
+            i = [4, SITE]
+        elif DESCRIPTION in params:
+            i = [5, DESCRIPTION]
+        par = {i[1]: r[i[0]]}
+        sql = 'DELETE FROM {0} WHERE {1}=:{1}'.format(TABLE, i[1])
+        return sql, par
+
+    def edit(self, *args, **kwargs):
+        pass
+
+
 class Password(object):
 
     email = None
@@ -19,7 +138,7 @@ class Password(object):
     description = None
     site = None
 
-    def create_pass(self, arguments):
+    def __call__(self, arguments):
 
         if isinstance(arguments, tuple):
             self.email = arguments[1]
@@ -33,31 +152,13 @@ class Password(object):
             self.login = arguments.l
             self.site = arguments.s
             self.description = arguments.d
+        return self
 
 
 class PasswordManager(object):
 
     def __init__(self, *args, **kwargs):
-
-        with sqlite3.connect('pass.db') as con:
-
-            self.con = con
-            cur = con.cursor()
-            try:
-                cur.execute('SELECT * FROM {}'.format(TABLE))
-            except sqlite3.OperationalError as e:
-                print(e)
-                query = 'CREATE TABLE IF NOT EXISTS {0} (id INTEGER PRIMARY KEY ASC, {1} text, {2} text, {3} text, {4} text, {5} text)'.format(
-                    TABLE, EMAIL, PASSWORD, LOGIN, SITE, DESCRIPTION
-                )
-                cur.execute(query)
-            con.commit()
-            self.cursor = cur
-
-    def __del__(self, *args, **kwargs):
-
-        self.cursor.close()
-        self.con.close()
+        self.db = Db()
 
     def clipboard(self, text):
 
@@ -78,62 +179,17 @@ class PasswordManager(object):
         else:
             raise Exception('Program requires the xclip or xsel application')
 
-    def parse_results(self, results):
-
-        res_list = {}
-        i = 1
-        for res in results:
-            res_list[i] = res
-            passw = Password()
-            print(res, type(res))
-            passw.create_pass(res)
-            print(
-                '{0}. email:{1}, login:{2}, site:{3}, description:{4}'.format(
-                    i, passw.email, passw.login, passw.site, passw.description
-                )
-            )
-            i += 1
-        return res_list
-
-    def search_query(self, params):
-
-        sql_get = 'SELECT * FROM {}'.format(TABLE)
-
-        if params:
-            params_sql = ' WHERE'
-            i = False
-            for p in params:
-                if i:
-                    params_sql += ' OR'
-                params_sql += ' {0}=:{1}'.format(p, p)
-                i = True
-            sql_get += params_sql
-        sql_get += ';'
-
-        return sql_get
-
     def search(self, arguments, *args, **kwargs):
-
-        password = Password()
-        password.create_pass(arguments)
-
+        print(arguments)
         try:
-            input_type = arguments.input_type
+            no_input = arguments.no_input
         except AttributeError:
-            input_type = False
+            no_input = False
 
-        params = self.params(password)
-        sql_get = self.search_query(params)
-        cur = self.cursor
-        results = False
-
-        try:
-            cur.execute(sql_get, params)
-            results = cur.fetchall()
-        except lite.Error as e:
-            print(e, 'err')
-
-        if results and input_type:
+        params = self.params(Password()(arguments))
+        results = self.db.search(params=params)
+        print(results)
+        if results and not no_input:
             res_list = self.parse_results(results)
             queny = input('Please enter a number of record ')
             try:
@@ -146,48 +202,19 @@ class PasswordManager(object):
                 self.clipboard(res_list[queny][2])
             else:
                 print('Sorry, this record does not exist')
-        else:
+        elif not results:
             print('Sorry, this record does not exist')
+
         print('search results')
         return results
 
-    def add_query(self, params):
-
-        sql_add = 'INSERT INTO ' + TABLE
-        if params:
-            name = ''
-            values = ''
-            i = False
-            for p in params:
-                if i:
-                    name += ', '
-                    values += ', '
-                name += p
-                values += ':' + p
-                i = True
-            sql_add = sql_add + '(' + name + ') VALUES (' + values + ')'
-        sql_add += ';'
-        return sql_add
-
     def add(self, arguments, *args, **kwargs):
 
-        password = Password()
-        password.create_pass(arguments)
-
-        if not password.password:
-            return 'Password is required'
-
-        params = self.params(password)
-
-        sql_add = self.add_query(params)
-        cur = self.cursor
-
-        try:
-            cur.execute(sql_add, params)
-            self.con.commit()
-        except lite.Error as e:
-            print(e, 'error')
-        print('Record was added')
+        params = self.params(Password()(arguments))
+        if not params.get('password', False):
+            print('Password is required')
+            return
+        self.db.add(params=params)
 
     def params(self, password):
         params = {}
@@ -204,38 +231,29 @@ class PasswordManager(object):
             params[DESCRIPTION] = password.description
         return params
 
-    def delete_query(self, params, r):
+    def parse_results(self, results):
 
-        if EMAIL in params:
-            i = [1, EMAIL]
-        elif LOGIN in params:
-            i = [3, LOGIN]
-        elif SITE in params:
-            i = [4, SITE]
-        elif DESCRIPTION in params:
-            i = [5, DESCRIPTION]
-        par = {i[1]: r[i[0]]}
-        sql = 'DELETE FROM {0} WHERE {1}=:{1}'.format(TABLE, i[1])
-        return sql, par
+        res_list = {}
+        i = 1
+        for res in results:
+            res_list[i] = res
+            passw = Password()(res)
+            print(
+                '{0}. email:{1}, login:{2}, site:{3}, description:{4}'.format(
+                    i, passw.email, passw.login, passw.site, passw.description
+                )
+            )
+            i += 1
+        return res_list
 
     def delete(self, arguments, *args, **kwargs):
-        password = Password()
-        password.create_pass(arguments)
+
         rows = self.search(arguments)
 
         if rows:
-            params = self.params(password)
+            params = self.params(Password()(arguments))
             if params:
-                cur = self.cursor
-
-                for r in rows:
-                    sql, par = self.delete_query(params, r)
-                    try:
-                        cur.execute(sql, par)
-                    except lite.Error as e:
-                        print(e)
-                self.con.commit()
-        print('delete pass')
+                self.db.delete(params=params, rows=rows)
 
     def edit(self, arguments, *args, **kwargs):
         rows = self.search(arguments)
@@ -294,7 +312,7 @@ def parse_args():
     parser_delete = subparsers.add_parser(
         'del',
         help='Delete password from database')
-    parser_delete.set_defaults(func=password.delete)
+    parser_delete.set_defaults(func=password.delete, no_input=True)
 
     parser_search = subparsers.add_parser(
         'srch',
